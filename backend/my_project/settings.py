6279511d4cea780b322a19cf7bedd2b99f3211ac
ArudhaGoudar <arudhagoudar@gmail.com>
2025-11-10 09:19:13 +0530

    Switch database configuration to use dj_database_url

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -84,11 +84,9 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
